8592fdf8dbfb687589d9a4c5b1b8d4760e2a2f2b
mrbmadrid <mrbmadrid@gmail.com>
2018-04-07 13:34:24 -0700

    Added full form validation functionality, up to Hacker challenge.

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, redirect, render_template, request, session, flash
+import datetime
+from datetime import datetime
 app = Flask(__name__)
 app.secret_key = '12AB3FEE4589'
 @app.route('/')
@@ -7,22 +9,35 @@ def index():
 
 @app.route('/results')
 def results():
-	return render_template('results.html')
+	return render_template('results.html', name=session['name'])
 
 @app.route('/submission', methods=['POST'])
 def submission():
-	if len(request.form['name']) == 0:
-		flash("Name field must not be empty.")
+	fields = {'first':'First Name', 'last':'Last Name', 'dob':'Date of Birth', 'password':'Password', 'confirm':'Confirm Passowrd', 'email':'Email'}
+	valid = True
+	for item in request.form:
+		if len(request.form[item]) == 0:
+			flash(fields[item] + " field must not be empty.")
+			valid = False
+	if request.form['password'] != request.form['confirm']:
+		flash("Passwords do not match.")
+		valid = False
+	if not any(char.isupper() for char in request.form['password']):
+		flash("Password must contain at least one upper case letter.")
+		valid = False
+	if not any(char.islower() for char in request.form['password']):
+		flash("Password must contain at least one lower case letter.")
+		valid = False
+	if not any(char.isdigit() for char in request.form['password']):
+		flash("Password must contain at least one number.")
+		valid = False
+	dob = request.form['dob'].split('-')
+	dob = datetime(int(dob[0]),int(dob[1]),int(dob[2]))
+	if not dob < datetime.now():
+		flash("You cannot be born in the future, check your date of birth.")
+		valid = False
+	if not valid:
 		return redirect('/')
-	session['name']=request.form['name']
-	session['location']=request.form['location']
-	session['favorite']=request.form['favorite_language']
-	if len(request.form['comment']) == 0:
-		flash("Comment field must not be empty.")
-		return redirect('/')
-	elif len(request.form['comment']) > 120:
-		flash("Comment must be no more than 120 characters.")
-		return redirect('/')
-	session['comment']=request.form['comment']
+	session['name'] = request.form['first']
 	return redirect('/results')
 app.run(debug=True)
